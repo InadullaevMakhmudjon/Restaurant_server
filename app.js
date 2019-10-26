a6ea30676de7c139b27efc596624869b6d7ace10
InadullaevMakhmudjon <33690336+InadullaevMakhmudjon@users.noreply.github.com>
2019-10-27 02:32:28 +0500

    front-end connected

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(logger('dev'));
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(join(__dirname, 'public')));
+app.use(express.static(join(__dirname, './client/build')));
 app.use('/images', express.static('images'));
 
 indexRouter(app);
